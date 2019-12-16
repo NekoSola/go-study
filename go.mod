d8062771e0b381ab2601bb56f302027e497c683d
NekoSola <361390990@qq.com>
2019-12-16 13:53:45 +0800

    fix

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/NekoSola/go-study
-
-go 1.12
-
-require golang.org/x/tools/gopls v0.1.7 // indirect
--- src/main/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/NekoSola/go-study/src/app"
-)
-
-func main() {
-	app.Hlwd()
-}
